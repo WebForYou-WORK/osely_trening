e7f23576034539aaac3bd1375dd97f2e294e4ed6
Andrej Melnichuk <omich2007@gmail.com>
2018-06-14 23:34:02 +0300

    Mobile services

--- js/fixedMenu.js
+++ js/fixedMenu.js
@@ -1,10 +1,10 @@
 $(function(){
     $(window).scroll(function() {
-        if($(this).scrollTop() >= 290) {
-            $('nav.nav_menu').addClass('stickytop');
+        if($(this).scrollTop() >= 200) {
+            $('.nav').addClass('stickytop');
         }
-    else{
-            $('nav.nav_menu').removeClass('stickytop');
+        else{
+            $('.nav').removeClass('stickytop');
         }
     });
 });
